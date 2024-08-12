0715cb461ced44bdfeb8b31302c5a7a95cd86d7f
riemann <aleqvids@gmail.com>
2024-08-12 22:13:41 +0300

    feat: match order example

--- src/bin/match_orders.rs
+++ src/bin/match_orders.rs
@@ -7,8 +7,8 @@ use fuels::{
 };
 use std::str::FromStr;
 
+use anyhow::Result;
 use spark_market_sdk::{MarketContract, OrderType};
-use std::error::Error;
 
 pub fn format_value_with_decimals(value: u64, decimals: u32) -> u64 {
     value * 10u64.pow(decimals)
@@ -19,7 +19,7 @@ pub fn format_to_readable_value(value: u64, decimals: u32) -> f64 {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
     dotenv().ok();
 
     // Environment variables
@@ -31,7 +31,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let main_wallet =
         WalletUnlocked::new_from_mnemonic_phrase(&mnemonic, Some(provider.clone())).unwrap();
-    let contract_id = ContractId::from_str(&contract_id)?;
+    let contract_id = ContractId::from_str(&contract_id).unwrap();
     let market = MarketContract::new(contract_id.clone(), main_wallet.clone()).await;
 
     // Fuel wallet address
@@ -42,7 +42,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let usdc_id: String = env::var("USDC_ID")?;
 
     // Getting asset balances
-    if let Some(account) = market.account(wallet_id).await.unwrap().value {
+    if let Some(account) = market.account(wallet_id).await?.value {
         let liquid_base = account.liquid.base;
         let liquid_quote = account.liquid.quote;
 
@@ -53,37 +53,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // Depositing Assets
-    let btc_id = AssetId::from_str(&btc_id)?;
-    let btc_amount = format_value_with_decimals(1, 8);
+    let btc_id = AssetId::from_str(&btc_id).unwrap();
+    let btc_amount = format_value_with_decimals(1, 7);
 
-    let usdc_id = AssetId::from_str(&usdc_id)?;
-    let usdc_amount = format_value_with_decimals(10_000, 6);
+    let usdc_id = AssetId::from_str(&usdc_id).unwrap();
+    let usdc_amount = format_value_with_decimals(10_000, 5);
 
     println!("Depositing BTC");
-    match market.deposit(btc_amount, btc_id).await {
-        Ok(_) => {
-            println!("Deposit Success");
-            Ok(())
-        }
-        Err(e) => {
-            print!("Deposit error: {:?}", e);
-            Err(e)
-        }
-    }
-    .unwrap();
+    market.deposit(btc_amount, btc_id).await?;
+    println!("Deposit Success");
 
     println!("Depositing USDC");
-    match market.deposit(usdc_amount, usdc_id).await {
-        Ok(_) => {
-            println!("Deposit Success");
-            Ok(())
-        }
-        Err(e) => {
-            print!("Deposit error: {:?}", e);
-            Err(e)
-        }
-    }
-    .unwrap();
+    market.deposit(usdc_amount, usdc_id).await?;
+    println!("Deposit Success");
 
     // Creating Buy / Sell Limit Orders
 
@@ -97,17 +79,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         format_to_readable_value(buy_amount, 6),
         format_to_readable_value(price, 9)
     );
-    match market.open_order(buy_amount, order_type, price).await {
-        Ok(_) => {
-            println!("Open Buy Order Success");
-            Ok(())
-        }
-        Err(e) => {
-            print!("Open Buy Order Error: {:?}", e);
-            Err(e)
-        }
-    }
-    .unwrap();
+    let order_id0 = market
+        .open_order(buy_amount, order_type, price)
+        .await?
+        .value;
 
     // Selling 0.1 BTC for 70k USDC
     let sell_amount: u64 = btc_amount;
@@ -119,20 +94,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
         format_to_readable_value(sell_amount, 8),
         format_to_readable_value(price, 9)
     );
-    match market.open_order(sell_amount, order_type, price).await {
-        Ok(_) => {
-            println!("Open Sell Order Success");
-            Ok(())
-        }
-        Err(e) => {
-            print!("Open Sell Order Error: {:?}", e);
-            Err(e)
-        }
-    }
-    .unwrap();
+    let order_id1 = market
+        .open_order(sell_amount, order_type, price)
+        .await?
+        .value;
 
-    let orders = market.user_orders(wallet_id).await?.value;
-    println!("orders {:?}", orders);
+    // Matching the two orders
+    println!("Matching Orders: {:?} and {:?}", order_id0, order_id1);
+    market.match_order_pair(order_id0, order_id1).await?;
+
+    println!("Orders Matched Successfully");
 
     Ok(())
 }
