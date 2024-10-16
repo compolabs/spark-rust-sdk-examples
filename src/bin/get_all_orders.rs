13db1771650714c36ce6846b529f3596acc45491
riemann <aleqvids@gmail.com>
2024-10-16 10:48:49 +0300

    feat: bump fuels-rs

---
+++ src/bin/get_all_orders.rs
@@ -0,0 +1,49 @@
+use dotenv::dotenv;
+use std::env;
+
+use fuels::{
+    accounts::provider::Provider, accounts::wallet::WalletUnlocked, types::ContractId,
+    types::Identity,
+};
+use std::str::FromStr;
+
+use spark_market_sdk::SparkMarketContract;
+use std::error::Error;
+
+pub fn format_value_with_decimals(value: u64, decimals: u32) -> u64 {
+    value * 10u64.pow(decimals)
+}
+
+pub fn format_to_readble_value(value: u64, decimals: u32) -> f64 {
+    value as f64 / 10u64.pow(decimals) as f64
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+
+    // Environment variables
+    let mnemonic = env::var("MNEMONIC")?;
+    let contract_id = env::var("ETH_USDC_CONTRACT_ID")?;
+
+    // Connect to provider
+    let provider = Provider::connect("testnet.fuel.network").await?;
+
+    let main_wallet =
+        WalletUnlocked::new_from_mnemonic_phrase(&mnemonic, Some(provider.clone())).unwrap();
+    let contract_id = ContractId::from_str(&contract_id)?;
+    let market = SparkMarketContract::new(contract_id.clone(), main_wallet.clone()).await;
+
+    // Fuel wallet address
+    let wallet_id: Identity = main_wallet.address().into();
+    println!("wallet {:?}", main_wallet.address().to_string());
+
+    // Fetching user orders
+    let orders = market.user_orders(wallet_id).await?.value;
+    println!("User Orders: {:?}", orders.len());
+
+    let account = market.account(wallet_id.clone()).await?.value;
+    println!("BTC account before: {:?}", account);
+
+    Ok(())
+}
