d69bf74a88069e74e13801f0bf74e941c470da1c
alexey <alexnagorny.an@gmail.com>
2024-10-28 19:12:09 +0000

    added airdrop script

---
+++ src/bin/airdrop.rs
@@ -0,0 +1,81 @@
+use dotenv::dotenv;
+use std::env;
+
+use fuels::{
+    accounts::{provider::Provider, wallet::WalletUnlocked, Account},
+    types::{bech32::Bech32Address, transaction::TxPolicies, Address},
+};
+use std::str::FromStr;
+
+use std::error::Error;
+
+const ASSET_ID: &str = "0x0000000000000000000000000000000000000000000000000000000000000000";
+const AMOUNT: u64 = 100;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+
+    // Environment variables
+    let mnemonic = env::var("MNEMONIC")?;
+    let provider = Provider::connect("testnet.fuel.network").await?;
+
+    let main_wallet =
+        WalletUnlocked::new_from_mnemonic_phrase(&mnemonic, Some(provider.clone())).unwrap();
+
+    let addresses = read_addresses_from_file("addresses")?;
+
+    println!(
+        "Starting airdrop of {} tokens to {} addresses",
+        AMOUNT,
+        addresses.len()
+    );
+
+    for address in addresses {
+        match main_wallet
+            .transfer(
+                &Bech32Address::from(Address::from_str(&address).unwrap()),
+                AMOUNT,
+                ASSET_ID.parse().unwrap(),
+                TxPolicies::default(),
+            )
+            .await
+        {
+            Ok(_) => {
+                println!("Successfully sent {} tokens to {}", AMOUNT, address);
+            }
+            Err(e) => {
+                println!("Failed to send tokens to {}: {}", address, e);
+            }
+        }
+    }
+
+    println!("Airdrop completed!");
+
+    Ok(())
+}
+
+//Файл должен содержать адреса Fuel кошельков, по одному адресу на строку. Пустые строки игнорируются.
+pub fn read_addresses_from_file(file_path: &str) -> Result<Vec<String>, std::io::Error> {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let addresses: Vec<String> = reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| line.trim().to_string())
+        .filter(|line| !line.is_empty())
+        .collect();
+
+    Ok(addresses)
+}
+
+pub fn format_value_with_decimals(value: u64, decimals: u32) -> u64 {
+    value * 10u64.pow(decimals)
+}
+
+pub fn format_to_readble_value(value: u64, decimals: u32) -> f64 {
+    value as f64 / 10u64.pow(decimals) as f64
+}
